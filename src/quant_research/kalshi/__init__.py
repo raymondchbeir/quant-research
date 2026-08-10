f313869534f0e18823cb4a1416c9153df250e74a
raymondchbeir <rchbeir@berkeley.edu>
2026-08-09 22:54:55 -0700

    Expose standalone recorder and primary shadow controls

--- src/quant_research/kalshi/__init__.py
+++ src/quant_research/kalshi/__init__.py
@@ -1,5 +1,5 @@
 from .live import live_status, start_live, stop_live
 from .recorder import current_session_dir, preview_15m_markets, recorder_status, start_recorder, stop_recorder
-from .primary_shadow_trader import shadow_status, start_shadow_trader, stop_shadow_trader
+from .primary_shadow_trader import primary_shadow_status, shadow_status, start_primary_shadow_trader, start_shadow_trader, stop_primary_shadow_trader, stop_shadow_trader
 
-__all__=["start_live","live_status","stop_live","preview_15m_markets","start_recorder","stop_recorder","recorder_status","current_session_dir","start_shadow_trader","stop_shadow_trader","shadow_status"]
+__all__=["start_live","live_status","stop_live","preview_15m_markets","start_recorder","stop_recorder","recorder_status","current_session_dir","start_primary_shadow_trader","stop_primary_shadow_trader","primary_shadow_status","start_shadow_trader","stop_shadow_trader","shadow_status"]
