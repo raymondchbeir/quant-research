6b5300d8a64ec1aca5128c74c3b019647eb41ac6
raymondchbeir <rchbeir@berkeley.edu>
2026-08-10 15:43:25 -0700

    Use v4 dashboard with M1-M5 strategy PnL

--- src/quant_research/kalshi/runtime_api.py
+++ src/quant_research/kalshi/runtime_api.py
@@ -125,7 +125,7 @@ def primary_shadow_status(show_rows=10):
 
 async def watch_primary_shadow_status(refresh_seconds=2.0, show_rows=8):
     _assert_pair_consistent()
-    from .shadow_dashboard_live_v3 import watch_primary_shadow_status as _watch
+    from .shadow_dashboard_live_v4 import watch_primary_shadow_status as _watch
     return await _watch(refresh_seconds=refresh_seconds, show_rows=show_rows)
 
 
