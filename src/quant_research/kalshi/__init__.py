bd78fb86cf64c27ceab3dd5c85115b984658cadc
raymondchbeir <rchbeir@berkeley.edu>
2026-08-09 20:57:14 -0700

    Add Kalshi live research package

---
+++ src/quant_research/kalshi/__init__.py
@@ -0,0 +1,9 @@
+from .live import live_status, start_live, stop_live
+from .recorder import current_session_dir, preview_15m_markets, recorder_status, start_recorder, stop_recorder
+from .shadow_trader import shadow_status, start_shadow_trader, stop_shadow_trader
+
+__all__ = [
+    "start_live", "live_status", "stop_live",
+    "preview_15m_markets", "start_recorder", "stop_recorder", "recorder_status", "current_session_dir",
+    "start_shadow_trader", "stop_shadow_trader", "shadow_status",
+]
