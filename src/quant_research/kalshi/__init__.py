280ea5715241b0cb6e2236fd32ebd0220bba22de
raymondchbeir <rchbeir@berkeley.edu>
2026-08-09 23:10:47 -0700

    Apply notebook parity patch before primary shadow use

--- src/quant_research/kalshi/__init__.py
+++ src/quant_research/kalshi/__init__.py
@@ -1,5 +1,6 @@
 from .live import live_status, start_live, stop_live
 from .recorder import current_session_dir, preview_15m_markets, recorder_status, start_recorder, stop_recorder
+from . import shadow_notebook_parity as _shadow_notebook_parity
 from .primary_shadow_trader import primary_shadow_status, shadow_status, start_primary_shadow_trader, start_shadow_trader, stop_primary_shadow_trader, stop_shadow_trader
 
 __all__=["start_live","live_status","stop_live","preview_15m_markets","start_recorder","stop_recorder","recorder_status","current_session_dir","start_primary_shadow_trader","stop_primary_shadow_trader","primary_shadow_status","start_shadow_trader","stop_shadow_trader","shadow_status"]
